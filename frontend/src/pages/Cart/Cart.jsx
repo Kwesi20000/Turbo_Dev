f275a4aea2d4c4bd1c7628f1dc4c8993b3187264
Kwesi20000 <hendrickskumi@yahoo.com>
2024-04-02 21:15:48 +0000

    hendy

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react'
 import './Cart.css'
+import StoreContextProvider from '../../context/StoreContext';
 
 const Cart = () => {
+
+  const {cartItems,food_list,removeFromCart} = useContext(StoreContextProvider);
+
+
   return (
     <div>
 
