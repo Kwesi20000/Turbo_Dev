440f7246a58cca7d7b6735d06e1c1d96e493b616
Kwesi20000 <hendrickskumi@yahoo.com>
2024-04-02 20:17:30 +0000

    hendy

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eum molestias, unde quasi ipsa magnam necessitatibus veritatis architecto. Consequuntur, sunt totam. Perspiciatis necessitatibus iste sit nulla excepturi, ratione nobis eos!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
