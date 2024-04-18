c92c81e39cda0dd92a3a26a93b6b76942b6a379f
Kwesi20000 <132357546+Kwesi20000@users.noreply.github.com>
2024-04-18 13:55:16 +0000

    hendy

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import Header from '../../components/Header/Header'
-import './Home.css'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../components/AppDownload/AppDownload'
+import { useState } from 'react';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import Header from '../../components/Header/Header';
+import './Home.css';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
+    // State to manage the selected category
+    const [category, setCategory] = useState("All");
 
-    const [category,setCategory] = useState("All")
-  return (
-    <div>
-        <Header/>
-        <ExploreMenu category={category} setCategory={setCategory}/>
-        <FoodDisplay  category={category} />
-        <AppDownload/>
-    </div>
-  )
+    return (
+        <div>
+            {/* Header component */}
+            <Header/>
+            {/* ExploreMenu component */}
+            <ExploreMenu category={category} setCategory={setCategory}/>
+            {/* FoodDisplay component */}
+            <FoodDisplay category={category} />
+            {/* AppDownload component */}
+            <AppDownload/>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
