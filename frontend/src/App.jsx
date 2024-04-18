c60d475182e82f3303c13963c3151e2790a168f3
Kwesi20000 <132357546+Kwesi20000@users.noreply.github.com>
2024-04-18 15:10:26 +0000

    📱 Add routing functionality for different pages (Home, Cart, PlaceOrder) and integrate Navbar, Footer, and LoginPopup components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom"; 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
