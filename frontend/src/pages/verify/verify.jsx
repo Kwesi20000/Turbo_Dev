25a696613273aac87d670a1c4c07ab0d1b65963f
Kwesi20000 <hendrickskumi@yahoo.com>
2024-07-18 00:49:24 +0000

    frontend

---
+++ frontend/src/pages/verify/verify.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './verify.css'
+
+const verify = () => {
+  return (
+    <div>
+        const [searchParams,setSearchParams] = usesea
+    </div>
+  )
+}
+
+export default verify
